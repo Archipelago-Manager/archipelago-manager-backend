348a8a5f8c244b656ab9b6679c201e5f24932e9c
NaabZer <anton.gefvert@gmail.com>
2025-03-21 22:56:47 +0100

    Add mock api call to node, and single game GET

--- app/api/routers/games.py
+++ app/api/routers/games.py
@@ -1,5 +1,6 @@
+from asyncio import sleep
 from typing import Annotated, List
-from fastapi import APIRouter, Query, HTTPException
+from fastapi import APIRouter, Query, HTTPException, BackgroundTasks
 from app.api.deps import SessionDep
 from app.models.games import Game, GameCreate, GamePublic, GamePublicWithUsers
 from app.models.users import User
@@ -8,12 +9,23 @@ from sqlmodel import select
 router = APIRouter(prefix="/games", tags=["games"])
 
 
+async def start_node_and_get_address(game: Game, session: SessionDep):
+    sleep(5)  # Mock API call to node to set up a server node
+    game.node_port = 38281
+    game.node_address = "localhost"
+    session.add(game)
+    session.commit()
+
+
 @router.post("/", response_model=GamePublic)
-def create_game(game: GameCreate, session: SessionDep):
+async def create_game(game: GameCreate, session: SessionDep,
+                      background_tasks: BackgroundTasks):
     db_game = Game.model_validate(game)
     session.add(db_game)
     session.commit()
     session.refresh(db_game)
+
+    background_tasks.add_task(start_node_and_get_address, db_game, session)
     return db_game
 
 
@@ -24,6 +36,14 @@ def read_games(session: SessionDep,
                ):
     games = session.exec(select(Game).offset(offset).limit(limit)).all()
     return games
+
+
+@router.get("/{game_id}", response_model=Game)
+def read_game(game_id: int, session: SessionDep):
+    game = session.get(Game, game_id)
+    if not game:
+        raise HTTPException(status_code=404, detail="Game not found")
+    return game
 
 
 @router.post("/{game_id}/add/{user_id}", response_model=GamePublicWithUsers)
