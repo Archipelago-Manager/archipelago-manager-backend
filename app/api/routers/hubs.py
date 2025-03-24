816be1972f3f743465cfb256404e9f4fb547b5c6
NaabZer <anton.gefvert@gmail.com>
2025-03-24 10:15:18 +0100

    Add some more hub api calls

--- app/api/routers/hubs.py
+++ app/api/routers/hubs.py
@@ -9,6 +9,12 @@ router = APIRouter(prefix="/hubs", tags=["hubs"])
 
 @router.post("/", response_model=HubPublic)
 def create_hub(hub: HubCreate, session: SessionDep):
+    hub = session.exec(select(Hub).where(Hub.name == hub.name))
+    if hub:
+        raise HTTPException(
+                status_code=400,
+                detail="A hub with this name already exists"
+                )
     db_hub = Hub.model_validate(hub)
     session.add(db_hub)
     session.commit()
@@ -28,6 +34,14 @@ def read_hubs(session: SessionDep,
 @router.get("/{hub_id}", response_model=HubPublic)
 def read_hub(hub_id: int, session: SessionDep):
     hub = session.get(Hub, hub_id)
+    if not hub:
+        raise HTTPException(status_code=404, detail="Hub not found")
+    return hub
+
+
+@router.get("/get_from_name/{hub_name}", response_model=HubPublic)
+def read_hub_from_name(hub_name: str, session: SessionDep):
+    hub = session.exec(select(Hub).where(Hub.name == hub_name)).first()
     if not hub:
         raise HTTPException(status_code=404, detail="Hub not found")
     return hub
