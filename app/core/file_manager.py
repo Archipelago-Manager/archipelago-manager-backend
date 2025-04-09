9cac6f772ed60ae0c99d9128f63edde00ec021fa
NaabZer <anton.gefvert@gmail.com>
2025-04-09 20:03:47 +0200

    Add more code s3 file handling

--- app/core/file_manager.py
+++ app/core/file_manager.py
@@ -1,5 +1,7 @@
 import boto3
+import tempfile
 from app.core.config import settings
+from typing import IO
 
 
 class FileManager():
@@ -15,26 +17,32 @@ class FileManager():
         else:
             self.storage_type = "local"
 
-    def write_s3(self, file):
+    def write_s3(self, file: IO) -> None:
+        self.s3.Bucket(settings.AWS_BUCKET_NAME).upload_fileobj(
+                Fileobj=file, Key=file.name
+                )
+
+    def write_local(self, file: IO, root_folder_name: str) -> None:
         pass
 
-    def write_local(self, file):
-        pass
-
-    def write(self, file):
+    def write(self, file: IO) -> None:
         if self.storage_type == "aws":
             self.write_s3(file)
         else:
             self.write_local(file)
 
-    def read_s3(self, file):
-        pass
+    def read_s3(self, file_name: str) -> IO:
+        tf = tempfile.TemporaryFile()
+        self.s3.Bucket(settings.AWS_BUCKET_NAME).download_fileobj(file_name,
+                                                                  tf)
+        return tf
 
-    def read_local(self, file):
-        pass
+    def read_local(self, file_name: str) -> IO:
+        tf = tempfile.TemporaryFile()
+        return tf
 
-    def read(self, file):
+    def read(self, file_name: str) -> IO:
         if self.storage_type == "aws":
-            self.read_s3(file)
+            return self.read_s3(file_name)
         else:
-            self.read_local(file)
+            return self.read_local(file_name)
